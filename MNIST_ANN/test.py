fed2c6fe49a0e6479da9423d608fac66c1bccbec
TAFIK El Mehdi <58373039+Enki-Dot@users.noreply.github.com>
2020-08-17 00:35:59 +0200

    Update ANN

--- MNIST_ANN/test.py
+++ MNIST_ANN/test.py
@@ -1,13 +1,17 @@
+#Load libraries:
 import numpy
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 from keras.datasets import mnist
 
+#Load model:
 model = tf.keras.models.load_model('ANN_Model.h5')
 
+#Load Dataset:
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+#Test model:
 t = x_test[6].reshape(1,784,)
 img = x_test[6].reshape((28,28))
 p = model.predict_classes(t)
